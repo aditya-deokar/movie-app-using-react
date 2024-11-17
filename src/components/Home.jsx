911a0279a084b14bea7654d75d122c8171e51232
adiya-deokar <adiyadeokar80@gmail.com>
2024-11-17 12:37:00 +0530

    hero section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
+import axios from "../utils/axios";
+import Hero from "../templetes/Hero";
 import SideNav from "../templetes/SideNav";
 import TopNav from "../templetes/TopNav";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+      document.title="Movie App | Home Page";
 
-    document.title="Movie App | Home Page";
+      const [TrendingMovie, setTrendingMovie] = useState([]);
+
+
+      async function GetTrendingMovie(){
+        try{
+          const { data } = await axios.get("https://api.themoviedb.org/3/trending/all/day");
+          console.log(data);
+
+          setTrendingMovie(data.results[Math.floor(Math.random()*20)])
+
+    
+          }catch(error){
+            console.log("error",error);
+          }
+    
+      }
+      
+      useEffect(() => {
+         GetTrendingMovie();
+      }, [])
+      
+
 
   return (
     <section className="w-full h-full flex ">
@@ -12,6 +37,9 @@ const Home = () => {
      
         <div className="w-[80%] h-screen">
             <TopNav/>
+             <Hero data={TrendingMovie} />
+            
+           
         </div>
     </section>
     
